10ecd7727a37d36a2d819c1a761fc6a75d6016e2
Oleksii Aloshkin <65122207+aleksusdesign@users.noreply.github.com>
2025-03-19 05:37:46 +0300

    feat: added playPage

---
+++ pages/playPage.page.ts
@@ -0,0 +1,32 @@
+import { Locator, Page } from "@playwright/test";
+import { User } from "types/users/users.type";
+import { BasePage } from "@pages/basePage";
+import { GameControls } from "@pages/components/gameControls.component";
+import { LoginForm } from "@pages/components/loginForm.component";
+
+export class PlayPage extends BasePage {
+  readonly gameControls: GameControls = new GameControls(this.page);
+  readonly loginForm: LoginForm = new LoginForm(this.page);
+  private loginButton: Locator = this.page.getByText('Login');
+  private readonly characterNameInput: Locator = this.page.getByPlaceholder('Galactic space lord');
+  private readonly startButton: Locator = this.page.getByRole('button', { name: 'Start!' });
+  public readonly levelStatSpan: Locator = this.page.locator('div[data-character-stats="Level"] span');
+
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async goto() {
+    await this.page.goto('/play');
+  }
+
+  async login(user: User) {
+    await this.loginButton.click();
+    await this.loginForm.login(user);
+  }
+
+  async createCharacter(characterName: string) {
+    await this.characterNameInput.fill(characterName);
+    await this.startButton.click();
+  }
+}
